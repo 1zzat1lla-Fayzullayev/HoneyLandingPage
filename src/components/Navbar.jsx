8f8d33fbe252f39f128d1d1cacd0d7a0ec068fff
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-01-10 10:37:56 +0500

    navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "../sass/_navbar.scss";
 
 function Navbar() {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
   return (
     <>
-      <div className="navbar">
-        <p>Honey</p>
+      <div>
         <nav>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Our Product</li>
-            <li>Contact</li>
+          <div className="logo">
+            <h3>Honey</h3>
+          </div>
+          <ul className={`nav-links ${showNavbar && "active"}`}>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Our Product</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
           </ul>
+
+          <div className="burger" onClick={handleShowNavbar}>
+            <div className="line-1"></div>
+            <div className="line-2"></div>
+            <div className="line-3"></div>
+          </div>
         </nav>
       </div>
     </>
