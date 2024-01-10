507bd50b226073f6a290565f6e53c5c031f5bbff
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-01-10 14:32:25 +0500

    configggg

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,44 @@
+import React, { useState } from "react";
+import "../sass/_navbar.scss";
+import navbar_logo from "../assets/images/navbar_logo.png";
 
-const Navbar = () => {
+function Navbar() {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <div className="navbar">
+        <nav>
+          <div className="logo">
+            <img src={navbar_logo} alt="navbar logo" />
+          </div>
+          <ul className={`nav-links ${showNavbar && "active"}`}>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Our Product</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+
+          <div className="burger" onClick={handleShowNavbar}>
+            <div className="line-1"></div>
+            <div className="line-2"></div>
+            <div className="line-3"></div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
